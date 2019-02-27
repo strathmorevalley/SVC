6f93aefb49a4b1e26082cb25373164818ea735e0
Unknown <college@anysupport.co.uk>
2019-02-27 14:34:45 +0000

    Updates

--- sql/svc_db.sql
+++ sql/svc_db.sql
@@ -1,22 +1,63 @@
 DROP DATABASE strathmore;
 CREATE DATABASE strathmore;
 
--- clear table (TRUNCATE) --
--- truncate table "tablename"
+truncate table users;
+truncate table lecturers;
 truncate table students;
+truncate table comments;
 
--- delete table (DROP) --
--- drop table "tablename"
+drop table users;
+drop table lecturers;
 drop table students;
+drop table comments;
 
--- student table --
 CREATE TABLE students(
     id INT(8) UNSIGNED AUTO_INCREMENT PRIMARY KEY
 ,	firstName VARCHAR(30) NOT NULL
 ,   lastName VARCHAR(50) NOT NULL
 ,   dob DATE
 ,   email VARCHAR(128) NOT NULL
-,	uid VARCHAR(7) UNIQUE NOT NULL
+,	uid INT(8) UNSIGNED UNIQUE NOT NULL
 ,	campusID VARCHAR(8)
 ,   password VARCHAR(32) NOT NULL
 );
+
+CREATE TABLE lecturers(
+    id INT(8) UNSIGNED AUTO_INCREMENT PRIMARY KEY
+,	firstName VARCHAR(30) NOT NULL
+,   lastName VARCHAR(50) NOT NULL
+,   dob DATE
+,   email VARCHAR(128) NOT NULL
+,	uid INT(8) UNSIGNED UNIQUE NOT NULL
+,	campusID VARCHAR(8)
+,   password VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE users(
+    id INT(8) UNSIGNED AUTO_INCREMENT PRIMARY KEY
+,	firstName VARCHAR(30) NOT NULL
+,   lastName VARCHAR(50) NOT NULL
+,   dob DATE
+,   email VARCHAR(128) NOT NULL
+,	utype VARCHAR(1)
+, 	CHECK (utype='l' OR utype='L' OR utype='s' OR utype='S')
+,	uid INT(8) UNSIGNED UNIQUE NOT NULL
+,	campusID VARCHAR(8)
+,   password VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE comments (
+	id INT(8) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
+,	date datetime NOT NULL
+,   message TEXT NOT NULL
+,	uid INT(8) UNSIGNED UNIQUE NOT NULL
+);
+
+ALTER TABLE `comments` 
+ADD CONSTRAINT `UsersComments_FK` 
+FOREIGN KEY (`uid`) REFERENCES `users`(`id`) 
+ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE users AUTO_INCREMENT=1230001;
+
+-- ********************************** --
